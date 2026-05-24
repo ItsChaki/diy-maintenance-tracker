d8581af4301fb28a3735516eabfb9d8a3e9af14a
Andrew Morales <andrewmorales477@gmail.com>
2026-05-24 11:37:26 -0700

    initial project scaffolding: gitignore, schema, dependencies

---
+++ schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE:
+
+Table ServiceRD {
+  id integer [primary key],
+  serviceRecordID integer,
+  serviceType string
+  productUsed varchar
+  quantity integer
+  costs integer
+  notes varcahr
+}
+
+Table Vehicle {
+  id integer [primary key]
+  vin integer
+  year integer
+  make string
+  model string
+  trim varchar
+  engine_size varchar
+  nickname varchar
+}
+
+Table ServiceRecord {
+  id integer [primary key]
+  vehicleID integer
+  service_date integer
+  mileage integer
+  isDiy boolean
+  serviceCenter varchar
+  totalcosts integer
+  notes varchar
+}
