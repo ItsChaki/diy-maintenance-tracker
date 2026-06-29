6dd47ce0bf95e189175bd4f73e5c7eaa38d0bd27
Andrew Morales <andrewmorales477@gmail.com>
2026-06-28 20:25:08 -0700

    added test function for updating vehicle name

--- test_vehicles.py
+++ test_vehicles.py
@@ -2,7 +2,7 @@
 import sqlite3
 import pytest
 import database
-from vehicles import add_vehicle, get_vehicle, list_vehicles
+from vehicles import add_vehicle, get_vehicle, list_vehicles, update_vehicle_nickname
 
 @pytest.fixture
 def test_db(tmp_path):
@@ -58,8 +58,50 @@ def test_list_vehicles(test_db):
     """
     Adds vehicles to the database, make sure lengths match
     """
+    #add 3 vehicles
+    add_vehicle(
+        vin="1HGBH41JXMN109186",
+        year=2020,
+        make="Toyota",
+        model="Corolla",
+        nickname="Girlfriend's Car",
+    )
+    add_vehicle(
+        vin="1HGBH41JXMN155387",
+        year=2013,
+        make="Nissan",
+        model="Altima",
+        nickname="Helltima",
+    )
+    add_vehicle(
+        vin="1HGBH41JXMN482950",
+        year=2017,
+        make="Nissan",
+        model="Maxima",
+        nickname="Dad's car",
+    )
+
+    #result comes back as list of tuples
     testList = list_vehicles()
-    assert testList[0] == "chaki"
+    assert len(testList) == 3
+
+
+def test_update_vehicle_name(test_db):
+    """
+    Add a vehicle to the test_db, update nickname, verify nickname has been changed
+    """
+    #get vehicle id
+    new_id = add_vehicle(
+        vin="1HGBH41JXMN155387",
+        year=2013,
+        make="Nissan",
+        model="Altima",
+        nickname="Helltima",
+    )
+    #update name and then get row given id
+    update_vehicle_nickname(new_id, "Chaki's Car")
+    test = get_vehicle(new_id)
+    assert test[7] == "Chaki's Car"
 
 
 
